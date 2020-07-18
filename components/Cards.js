b5a622be29750dce34ccdfcdc79b90f057382772
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-07-18 14:43:21 -0400

    complete MVP... not enough time for Stretch

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,57 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response);
+        
+        // const keys = Object.keys(response.data.articles);
+        const values = Object.values(response.data.articles);
+        console.log(values);
+        const cardContent = document.querySelector('.cards-container');
+        for(let i in values){
+            let value = values[i];
+            for(i = 0; i < value.length; i++){
+                cardContent.append(ArticleMaker(value[i]));
+            }
+        }
+    })
+    .catch(error => {
+        console.log('Alert Missing Data ', error);
+    })
+
+function ArticleMaker(d){
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const headLine = document.createElement('div');
+    headLine.textContent = d.headline;
+    headLine.classList.add('headline');
+    card.append(headLine);
+
+    const author = document.createElement('div');
+    author.classList.add('author');
+    card.append(author);
+
+    const imgCont = document.createElement('div');
+    imgCont.classList.add('img-container');
+    author.append(imgCont);
+
+    const img = document.createElement('img');
+    img.src = d.authorPhoto;
+    imgCont.append(img);
+
+    const authorSign = document.createElement('span');
+    authorSign.textContent = d.authorName;
+    author.append(authorSign);
+
+    return card;
+}
+card = document.querySelector(".card");
+card.addEventListener('click', () => {
+    console.log(headline)
+
+    
+})
--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,27 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const header = document.createElement('div');
+    header.classList.add('header')
+
+    const date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = 'MARCH 28, 2020'
+    header.append(date);
+
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Lambda Times';
+    header.append(h1);
+
+    const temp = document.createElement('span');
+    temp.classList.add('temp');
+    temp.textContent = '98°';
+    header.append(temp);
+
+    return header
+}
+
+const headCont = document.querySelector('.header-container');
+headCont.append(Header());
+
